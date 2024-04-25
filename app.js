999cef5fd5b3615cf483d7aa33d32847fcbec1de
IrynaFuzhenko <158091863+IrynaFuzhenko@users.noreply.github.com>
2024-04-26 00:35:04 +0300

    Corrected Footer

--- app.js
+++ app.js
@@ -2,19 +2,17 @@
 const img = document.getElementById('thousand-01');
 const mediaQuery = window.matchMedia('(max-width: 600px)');
 
-// Додаємо обробник події для наведення миші
-img.addEventListener('mouseover', () => {
-    // Генеруємо випадкові координати для руху
-    const offsetX = Math.random() * 100 - 50; // Випадкове зміщення по горизонталі в межах [-50, 50]
-    const offsetY = Math.random() * 100 - 50; // Випадкове зміщення по вертикалі в межах [-50, 50]
 
-    // Застосовуємо трансформацію до зображення
+img.addEventListener('mouseover', () => {
+ 
+    const offsetX = Math.random() * 100 - 50; 
+    const offsetY = Math.random() * 100 - 50; 
+
+   
     img.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
 });
-
-// Додаємо обробник події для зняття миші
 img.addEventListener('mouseout', () => {
-    // При знятті миші повертаємо зображення в початкове положення
+
     img.style.transform = 'translate(0, 0)';
 });
 document.addEventListener('DOMContentLoaded', function() {
